d759d7b87adc48518582764705607a94284cb9f0
Ben Machin <ben@machingray.me.uk>
2025-11-27 20:42:15 +0000

    tweak

--- build_files/build.sh
+++ build_files/build.sh
@@ -10,14 +10,14 @@ dnf5 install -y wget alien python3-pip python3-devel gcc
 ### Download Coral runtime (Debian package for x86_64)
 wget https://packages.cloud.google.com/apt/pool/libe/libedgetpu/libedgetpu1-std_15.0_amd64.deb
 
-### Convert .deb to .rpm and install
+### Convert .deb to .rpm and install into the image
 alien -r libedgetpu1-std_15.0_amd64.deb
 rpm -ivh ./libedgetpu1-std-15.0-1.x86_64.rpm
 
-### Install Python bindings
+### Install Python bindings for Coral
 pip3 install pycoral
 
-### Add udev rules for Coral USB
+### Add udev rules for Coral USB devices
 cat <<'EOF' > /etc/udev/rules.d/99-edgetpu-accelerator.rules
 SUBSYSTEM=="usb", ATTR{idVendor}=="1a6e", MODE="0666", GROUP="plugdev"
 EOF
