68488b0819bcbbe611e2d38b54ad627ce02930ff
Ben Machin <ben@machingray.me.uk>
2025-11-27 20:08:19 +0000

    using container based config

--- build_files/build.sh
+++ build_files/build.sh
@@ -4,21 +4,19 @@ set -ouex pipefail
 
 ### Install packages
 
-# Packages can be installed from any enabled yum repo on the image.
-# RPMfusion repos are available by default in ublue main images
-# List of rpmfusion packages can be found here:
-# https://mirrors.rpmfusion.org/mirrorlist?path=free/fedora/updates/39/x86_64/repoview/index.html&protocol=https&redirect=1
+# Example: install tmux from Fedora repos
+dnf5 install -y tmux
 
-# this installs a package from fedora repos
-dnf5 install -y tmux 
+# Coral runtime and bindings
+dnf5 install -y \
+  libedgetpu1-std \
+  python3-pycoral \
+  libusb1
 
-# Use a COPR Example:
-#
-# dnf5 -y copr enable ublue-os/staging
-# dnf5 -y install package
-# Disable COPRs so they don't end up enabled on the final image:
-# dnf5 -y copr disable ublue-os/staging
+# Add udev rules for Coral USB devices
+cat <<'EOF' > /etc/udev/rules.d/99-edgetpu-accelerator.rules
+SUBSYSTEM=="usb", ATTR{idVendor}=="1a6e", MODE="0666", GROUP="plugdev"
+EOF
 
 #### Example for enabling a System Unit File
-
 systemctl enable podman.socket
