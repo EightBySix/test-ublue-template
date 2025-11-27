60143f14eaa719f04c4fc6959427072aa4439d0e
Ben Machin <ben@machingray.me.uk>
2025-11-27 20:21:08 +0000

    vendor in googles deb

--- build_files/build.sh
+++ build_files/build.sh
@@ -1,22 +1,26 @@
 #!/bin/bash
-
 set -ouex pipefail
 
-### Install packages
-
-# Example: install tmux from Fedora repos
+### Base example package
 dnf5 install -y tmux
 
-# Coral runtime and bindings
-dnf5 install -y \
-  libedgetpu1-std \
-  python3-pycoral \
-  libusb1
+### Dependencies for conversion and Python bindings
+dnf5 install -y wget alien python3-pip python3-devel gcc
 
-# Add udev rules for Coral USB devices
+### Download Coral runtime (Debian package)
+wget https://packages.cloud.google.com/apt/pool/libe/libedgetpu/libedgetpu1-std_15.0_amd64.deb
+
+### Convert .deb to .rpm and install
+alien -r libedgetpu1-std_15.0_amd64.deb
+rpm-ostree install ./libedgetpu1-std-15.0-1.x86_64.rpm
+
+### Install Python bindings
+pip3 install pycoral
+
+### Add udev rules for Coral USB
 cat <<'EOF' > /etc/udev/rules.d/99-edgetpu-accelerator.rules
 SUBSYSTEM=="usb", ATTR{idVendor}=="1a6e", MODE="0666", GROUP="plugdev"
 EOF
 
-#### Example for enabling a System Unit File 
+### Enable podman socket as in template
 systemctl enable podman.socket
